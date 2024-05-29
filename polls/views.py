46c5af4d547a40ba8a8706d9ba723256f1da7ff3
Justin Howe <jwhowe02@proton.me>
2024-05-29 00:24:51 -0700

    move templates for polls app to its own template directory, properly namespaced and rendered

--- polls/views.py
+++ polls/views.py
@@ -8,8 +8,12 @@ import datetime
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return render(request, "index.html", {"output": output})
+
+    context = {
+        "questions": latest_question_list
+    }
+
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
