387d72db6e1ccd38543339235656184e1d3ab19d
Justin Howe <jwhowe02@proton.me>
2024-07-01 09:21:30 -0700

    Moved site-wide template to its own root directory and created home page

--- django_sandbox/urls.py
+++ django_sandbox/urls.py
@@ -19,7 +19,10 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
+    path("", views.home, name="home"),
     path("polls/", include("polls.urls"), None, "polls"),
     path("admin/", admin.site.urls),
 ]
